6959d2139f15e5a3914970c10f58c2ae9e4ee98f
janchayla68-source <janchay.la.68@ubu.ac.th>
2026-01-30 16:54:20 +0700

    Update app2.py

--- Modals/app2.py
+++ Modals/app2.py
@@ -1,8 +1,1 @@
-from Modals.homework import BankAccount
 
-my_account = BankAccount(1000)
-your_account = BankAccount(500)
-
-our_account = my_account + your_account
-#our_account = our_account - your_account  
-print(our_account)
